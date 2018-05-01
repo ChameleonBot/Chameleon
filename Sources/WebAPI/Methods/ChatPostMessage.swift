2e8a036d09d6a1d876b7df4d552ee5fc637de4ff
Ian Keen <iankeen82@gmail.com>
2018-04-30 19:36:50 -0700

    Bugfix

--- Sources/WebAPI/Methods/ChatPostMessage.swift
+++ Sources/WebAPI/Methods/ChatPostMessage.swift
@@ -1,18 +1,21 @@
 
 public struct ChatPostMessage: WebAPIRequest {
-    public var baseUrl: String {
-        return message.response_url ?? DefaultBaseURL
+    public var url: String {
+        return message.response_url ?? WebAPIURL(base: DefaultBaseURL, endpoint)
+    }
+    public var encoding: Encoding {
+        return message.response_url == nil ? .form : .json
     }
     public let message: ChatMessage
     public let endpoint = "chat.postMessage"
     public var body: [String: Any?] {
         return message.encode()
     }
-
+    
     public init(message: ChatMessage) {
         self.message = message
     }
-
+    
     public func handle(response: NetworkResponse) throws -> Void {
         //
     }
