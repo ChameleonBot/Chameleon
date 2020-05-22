06ba0b16fce20b72140848f1cba1ff713fa6d298
Ian Keen <iankeen82@gmail.com>
2020-05-21 17:48:36 -0700

    Update parsers

--- Sources/ChameleonKit/Common/Parser/Parser+Numbers.swift
+++ Sources/ChameleonKit/Common/Parser/Parser+Numbers.swift
@@ -1,5 +1,9 @@
 extension Parser {
     public static var integer: Parser<Int> {
+        let parser = Parser.char("-").optional && .positiveInteger
+        return parser.map { $0 == nil ? $1 : $1 * -1 }
+    }
+    public static var positiveInteger: Parser<Int> {
         let notANumber = char(where: { !$0.isNumber }).void || .end
 
         return char(where: { $0.isNumber })
