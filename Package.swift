79c28f2e8ba00195db1e201fe274d4f27a981435
Ian Keen <iankeen82@gmail.com>
2020-05-23 12:07:11 -0700

    Updates

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         .macOS(.v10_15),
     ],
     products: [
+        .executable(name: "Chameleon", targets: ["Chameleon"]),
         .library(name: "ChameleonKit", targets: ["ChameleonKit"]),
         .library(name: "VaporProviders", targets: ["VaporProviders"]),
     ],
@@ -19,6 +20,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0"),
     ],
     targets: [
+        .target(name: "Chameleon", dependencies: ["ChameleonKit", "VaporProviders"]),
         .target(name: "ChameleonKit", dependencies: []),
         .target(name: "VaporProviders", dependencies: ["ChameleonKit", "Vapor", "HTTP", "URLEncodedForm", "Redis"]),
         .testTarget(name: "ChameleonKitTests", dependencies: ["ChameleonKit"]),
--- Sources/Chameleon/main.swift
+++ Sources/Chameleon/main.swift
@@ -6,17 +6,18 @@ let storage = PListStorage()
 let store = MemoryKeyValueStorage()
 
 store.set(value: <#VALUE#>, forKey: "VERIFICATION_TOKEN")
-store.set(value: <#VALUE#>, forKey: "API_TOKEN")
+store.set(value: <#VALUE#>, forKey: "BOT_ACCESS_TOKEN")
 
 let bot = try SlackBot
     .vaporBased(
-       verificationToken: try store.get(forKey: "VERIFICATION_TOKEN"),
-       accessToken: try store.get(forKey: "API_TOKEN")
+        verificationToken: try store.get(forKey: "VERIFICATION_TOKEN"),
+        accessToken: try store.get(forKey: "BOT_ACCESS_TOKEN")
     )
 
 bot.listen(for: .message) { bot, message in
     try message.matching("hello " && .user(bot.me)) { _ in
-    try bot.perform(.respond(to: message, .inline, with: "Hi!!"))
+        try bot.perform(.respond(to: message, .inline, with: "Hi!!"))
+    }
 }
 
 try bot.start()
