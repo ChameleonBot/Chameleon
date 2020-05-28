1e6668b348ae08a98c4aa19b1462c97edc9736ee
Ian Keen <iankeen82@gmail.com>
2020-05-28 12:39:59 -0700

    Reorganize SlackActions

--- Sources/ChameleonKit/Slack/Dispatcher/SlackAction+Misc.swift
+++ Sources/ChameleonKit/Slack/Dispatcher/SlackAction+Misc.swift
@@ -7,29 +7,6 @@ extension SlackAction {
 }
 
 extension SlackAction {
-    private struct UserPacket: Encodable {
-        let include_locale: Bool
-        let user: Identifier<User>
-    }
-
-    public static func user(_ identifier: Identifier<User>, include_locale: Bool = true) -> SlackAction<User> {
-        let packet = UserPacket(include_locale: include_locale, user: identifier)
-        return .init(name: "users.info", method: .post, encoding: .url, packet: packet)
-    }
-}
-
-extension SlackAction {
-    private struct ChannelPacket: Encodable {
-        let channel: Identifier<Channel>
-    }
-
-    public static func channel(_ identifier: Identifier<Channel>) -> SlackAction<Channel> {
-        let packet = ChannelPacket(channel: identifier)
-        return .init(name: "conversations.info", method: .post, encoding: .url, packet: packet)
-    }
-}
-
-extension SlackAction {
     private struct ReactionPacket: Encodable {
         let name: String
         let channel: Identifier<Channel>
