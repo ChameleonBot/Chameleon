37e9feeb7d0ce16ff7cf986f96d8184e66ecf63f
Ian Keen <iankeen82@gmail.com>
2020-06-08 17:43:06 -0700

    Add initial SlackBot integration tests

---
+++ Tests/ChameleonKitTests/Slack/SlackBotTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import ChameleonKit
+import ChameleonTestKit
+
+final class SlackBotTests: XCTestCase {
+    func testSlackEvents_Message() throws {
+        let test = try SlackBot.test()
+
+        var message: Message?
+        test.bot.listen(for: .message) { message = $1 }
+        try test.send(.event(.message("hello world")))
+
+        XCTAssertEqual(message?.text, "hello world")
+    }
+
+    func testParserMatching_ExactMatch_CaseInsensitive() throws {
+        let test = try SlackBot.test()
+        var count = 0
+
+        test.bot.listen(for: .message) { bot, message in
+            try message.matching("hello world") { count += 1 }
+            try message.matching("HELLO WORLD") { count += 1 }
+        }
+        try test.send(.event(.message("hello world")))
+        try test.send(.event(.message("HELLO WORLD")))
+
+        XCTAssertEqual(count, 4)
+    }
+}
+
