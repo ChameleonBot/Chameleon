47448271c5739464a7f32bf22727e987e768207d
Ian Keen <iankeen82@gmail.com>
2020-06-11 14:19:47 -0700

    Movee TeamsJoin to right place

--- Sources/ChameleonTestKit/Fixtures/EventAPI/Team.Join/TeamJoin+Fixture.swift
+++ Sources/ChameleonTestKit/Fixtures/EventAPI/Team.Join/TeamJoin+Fixture.swift
@@ -2,7 +2,7 @@ import ChameleonKit
 import Foundation
 
 extension FixtureSource {
-    public static func teamJoin(_ user: FixtureSource<User>) throws -> FixtureSource<SlackDispatcher> {
+    public static func teamJoin(_ user: FixtureSource<User>) throws -> FixtureSource<SlackReceiver> {
         return try .init(jsonFile: "TeamJoin", map: [.user: user.string()])
     }
 }
