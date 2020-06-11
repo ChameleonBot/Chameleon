6668b39e57a8791974bc661af3c970a02829aefc
Ian Keen <iankeen82@gmail.com>
2020-06-11 14:13:59 -0700

    Update TeamJoin fixture

--- Sources/ChameleonTestKit/Fixtures/WebAPI/Team.Join/TeamJoin+Fixture.swift
+++ Sources/ChameleonTestKit/Fixtures/WebAPI/Team.Join/TeamJoin+Fixture.swift
@@ -2,7 +2,7 @@ import ChameleonKit
 import Foundation
 
 extension FixtureSource {
-    public static func teamJoin(user: FixtureSource<User>) throws -> FixtureSource<SlackDispatcher> {
+    public static func teamJoin(_ user: FixtureSource<User>) throws -> FixtureSource<SlackDispatcher> {
         return try .init(jsonFile: "TeamJoin", map: [.user: user.string()])
     }
 }
