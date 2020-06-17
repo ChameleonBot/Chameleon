cbd4f228e2349e2bd495876acc9007ca6b527442
Ian Keen <iankeen82@gmail.com>
2020-06-17 15:19:59 -0700

    DSL update

--- Sources/ChameleonKit/Slack/Dispatcher/Posts/Builder/LayoutBlockBuilder/LayoutBlockBuilder+Context.swift
+++ Sources/ChameleonKit/Slack/Dispatcher/Posts/Builder/LayoutBlockBuilder/LayoutBlockBuilder+Context.swift
@@ -4,7 +4,7 @@ extension MessagesSurface: ContextContext { }
 extension HomeTabSurface: ContextContext { }
 
 extension LayoutBlockBuilder where Context: ContextContext {
-    public static func context(block_id: String? = nil, elements: [BlockElementBuilder<Message.Layout.Context>]) -> LayoutBlockBuilder {
+    public static func context(block_id: String? = nil, _ elements: [BlockElementBuilder<Message.Layout.Context>]) -> LayoutBlockBuilder {
         return .init {
             return Message.Layout.Context(type: Message.Layout.Context.type, elements: elements.map { $0.build() }, block_id: block_id)
         }
